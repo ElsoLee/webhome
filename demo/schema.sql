d2d21d9241e2e5d9ffcf9a5f325b8b4af9805fb5
colin <linke1355@163.com>
2013-08-06 09:52:11 +0800

    fix

--- demo/schema.sql
+++ demo/schema.sql
@@ -1,14 +1,7 @@
 -- Copyright 2013 webhome
 --
--- changhong contest
+-- changhong software contest
 --
-
--- To create the database:
--- CREATE DATABASE webhome;
--- GRANT ALL PRIVILEGES ON webhome. * TO 'blog'@'localhost' IDENTIFIED BY 'webhome';
---
--- To reload the tables:
--- mysql --user=webhome --password=blog --database=blog < schema.sql
 
 DROP TABLE IF EXISTS users;
 CREATE TABLE users (
@@ -16,4 +9,4 @@ CREATE TABLE users (
 	email VARCHAR(100) NOT NULL UNIQUE,
 	name VARCHAR(100) NOT NULL UNIQUE,
 	password VARCHAR(100) NOT NULL
-);
+ );
